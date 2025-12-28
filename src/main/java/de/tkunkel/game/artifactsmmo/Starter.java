d10dc4028bf24474881fd5be9ef72a44914e43e5
Torsten Kunkel <git@tkunkel.de>
2025-12-28 22:36:46 +0100

    enable caching

--- src/main/java/de/tkunkel/game/artifactsmmo/Starter.java
+++ src/main/java/de/tkunkel/game/artifactsmmo/Starter.java
@@ -4,9 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication(scanBasePackages = "de.tkunkel.game.artifactsmmo")
+@EnableCaching
 public class Starter {
     private final Logger logger = LoggerFactory.getLogger(Starter.class.getName());
 
