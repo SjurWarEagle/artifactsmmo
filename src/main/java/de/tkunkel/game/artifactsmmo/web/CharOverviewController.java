ce8b3d71e5e558fc86010d4342cf4a23c24354f9
Torsten Kunkel <git@tkunkel.de>
2026-01-04 05:21:53 +0100

    add docker image + github build

---
+++ src/main/java/de/tkunkel/game/artifactsmmo/web/CharOverviewController.java
@@ -0,0 +1,35 @@
+package de.tkunkel.game.artifactsmmo.web;
+
+import de.tkunkel.game.artifactsmmo.AdventureManager;
+import de.tkunkel.game.artifactsmmo.api.AccountsApiWrapper;
+import de.tkunkel.game.artifactsmmo.api.CharactersApiWrapper;
+import de.tkunkel.games.artifactsmmo.model.CharacterSchema;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class CharOverviewController {
+    private final Logger logger = LoggerFactory.getLogger(CharOverviewController.class.getName());
+    private final AdventureManager adventureManager;
+    private final AccountsApiWrapper accountsApiWrapper;
+    private final CharactersApiWrapper charactersApiWrapper;
+
+    public CharOverviewController(AdventureManager adventureManager, AccountsApiWrapper accountsApiWrapper, CharactersApiWrapper charactersApiWrapper) {
+        this.adventureManager = adventureManager;
+        this.accountsApiWrapper = accountsApiWrapper;
+        this.charactersApiWrapper = charactersApiWrapper;
+    }
+
+    @GetMapping("/chars")
+    public String showWishlist(Model model) {
+        List<CharacterSchema> characters = accountsApiWrapper.getAccountCharactersAccountsAccountCharactersGet()
+                                                             .getData();
+        model.addAttribute("characters", characters);
+        return "characters";
+    }
+}
