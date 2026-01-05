060d6a3ec3269caed114d37d26bbe50fd0dbe49a
Torsten Kunkel <git@tkunkel.de>
2026-01-05 22:53:52 +0100

    cleanup

--- src/main/java/de/tkunkel/game/artifactsmmo/brains/tier01/MinerT1Brain.java
+++ src/main/java/de/tkunkel/game/artifactsmmo/brains/tier01/MinerT1Brain.java
@@ -8,20 +8,18 @@ import de.tkunkel.game.artifactsmmo.brains.CommonBrain;
 import de.tkunkel.game.artifactsmmo.shopping.Wish;
 import de.tkunkel.game.artifactsmmo.shopping.WishList;
 import de.tkunkel.game.artifactsmmo.tasks.*;
-import de.tkunkel.games.artifactsmmo.model.*;
+import de.tkunkel.games.artifactsmmo.model.CharacterResponseSchema;
+import de.tkunkel.games.artifactsmmo.model.GatheringSkill;
+import de.tkunkel.games.artifactsmmo.model.Skill;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
 @Service
 public class MinerT1Brain extends CommonBrain {
     private final Logger logger = LoggerFactory.getLogger(MinerT1Brain.class.getName());
-    private final FarmHighestResourceTask farmHighestResourceTask;
-    private final CraftItemTask craftItemTask;
     private final TrainingSkillTask trainingSkillTask;
     private final BankDepositAllTask bankDepositAllTask;
 
@@ -31,109 +29,40 @@ public class MinerT1Brain extends CommonBrain {
                         BankFetchItemsAndCraftTask bankFetchItemsAndCraftTask, TrainingSkillTask trainingSkillTask,
                         CharHelper charHelper) {
         super(caches, wishList, apiHolder, charHelper, bankFetchItemsAndCraftTask);
-        this.farmHighestResourceTask = farmHighestResourceTask;
-        this.craftItemTask = craftItemTask;
         this.bankDepositAllTask = bankDepositAllTask;
         this.trainingSkillTask = trainingSkillTask;
     }
 
-    public boolean mineIfNotEnoughInInventory(String characterName, String oreName, int amount) {
-        CharacterResponseSchema character = apiHolder.charactersApi.getCharacterCharactersNameGet(characterName);
-        charHelper.waitUntilCooldownDone(character);
-
-        List<InventorySlot> inventory = character.getData()
-                                                 .getInventory();
-
-        int amountInInventory = inventory.stream()
-                                         .filter(inventorySlot -> inventorySlot.getCode()
-                                                                               .equals(oreName))
-                                         .mapToInt(InventorySlot::getQuantity)
-                                         .sum()
-                ;
-        if (amountInInventory >= amount) {
-            return true;
-        }
-        Optional<MapSchema> mine = findClosestLocation(character, "copper_rocks");
-        if (mine.isEmpty()) {
-            logger.error("No mine found");
-            return false;
-        }
-        charHelper.moveToLocation(character, mine.get());
-        gather(character);
-        logger.info("Starting mineUntilReached");
-        return true;
-    }
-
-    private void gather(CharacterResponseSchema character) {
-        charHelper.waitUntilCooldownDone(character);
-        apiHolder.myCharactersApi.actionGatheringMyNameActionGatheringPost(character.getData()
-                                                                                    .getName());
-
-    }
-
-    public void smelt(String characterName, String toCraft) {
-        CharacterResponseSchema character = apiHolder.charactersApi.getCharacterCharactersNameGet(characterName);
-        Optional<MapSchema> smelter = findClosestLocation(character, "mining");
-        if (smelter.isEmpty()) {
-            logger.error("No smelter found.");
-            throw new RuntimeException("no smelter found");
-        }
-        charHelper.waitUntilCooldownDone(character);
-        charHelper.moveToLocation(character, smelter.get());
-        charHelper.waitUntilCooldownDone(character);
-        CraftingSchema craftingSchema = new CraftingSchema().code(toCraft)
-                                                            .quantity(1);
-        apiHolder.myCharactersApi.actionCraftingMyNameActionCraftingPost(character.getData()
-                                                                                  .getName(), craftingSchema
-        );
-    }
-
-
     @Override
     public void runBaseLoop(String characterName) {
-        try {
-            CharacterResponseSchema character = apiHolder.charactersApi.getCharacterCharactersNameGet(characterName);
-            bankDepositAllTask.depositInventoryInBankIfInventoryIsFull(this, character);
-            charHelper.waitUntilCooldownDone(character);
-            equipOrRequestBestToolForSkill(character, "mining");
+        CharacterResponseSchema character = apiHolder.charactersApi.getCharacterCharactersNameGet(characterName);
+        charHelper.waitUntilCooldownDone(character);
+        bankDepositAllTask.depositInventoryInBankIfInventoryIsFull(this, character);
+        charHelper.waitUntilCooldownDone(character);
+        equipOrRequestBestToolForSkill(character, "mining");
 
-            Optional<Wish> wish = findPossibleItemToCraftFromWishlist(character);
+        Optional<Wish> wish = findPossibleItemToCraftFromWishlist(character);
 
-            if (wish.isPresent()) {
-                bankFetchItemsAndCraftTask.craftItemWithBankItems(this, character, wish.get().itemCode);
-                wish.get().reservedBy = null;
-                wish.get().fulfilled = true;
-            } else {
-                trainingSkillTask.trainSkills(character.getData(), Skill.MINING, Skill.GEARCRAFTING, Skill.WEAPONCRAFTING);
-
-                var itemToCraft = findPossibleItemToCraft(character);
-                if (itemToCraft.isPresent()) {
-                    craftItemTask.craftItem(this, characterName, itemToCraft.get());
-                } else {
-                    farmHighestResourceTask.farmResource(this, characterName);
-                }
-            }
-
-            Thread.sleep(TimeUnit.SECONDS.toMillis(1));
-        } catch (InterruptedException e) {
-            logger.error("Error while mining", e);
-            throw new RuntimeException(e);
+        if (wish.isPresent()) {
+            bankFetchItemsAndCraftTask.craftItemWithBankItems(this, character, wish.get().itemCode);
+            wish.get().reservedBy = null;
+            wish.get().fulfilled = true;
+        } else {
+            trainingSkillTask.trainSkills(character.getData(), Skill.MINING, Skill.GEARCRAFTING, Skill.WEAPONCRAFTING);
         }
     }
 
     private Optional<Wish> findPossibleItemToCraftFromWishlist(CharacterResponseSchema character) {
-        Optional<Wish> wish = wishList.reserveWishThatCanBeCraftedByMe(character);
-        return wish;
+        return wishList.reserveWishThatCanBeCraftedByMe(character);
     }
 
     @Override
     public String decideWhatResourceToFarm(String characterName) {
         CharacterResponseSchema character = apiHolder.charactersApi.getCharacterCharactersNameGet(characterName);
 
-        String resource = caches.findHighestFarmableResourceForSkillLevel(character.getData()
-                                                                                   .getMiningLevel(), GatheringSkill.MINING
+        return caches.findHighestFarmableResourceForSkillLevel(character.getData()
+                                                                        .getMiningLevel(), GatheringSkill.MINING
         );
-        return resource;
     }
 
 
